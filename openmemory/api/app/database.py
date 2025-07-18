5087f355979f2a854a132160641797db22049a2b
fhuzjan <fran.huzjan@ingemark.com>
2025-07-18 10:25:38 +0200

    DATABASE_URL mandatory

--- openmemory/api/app/database.py
+++ openmemory/api/app/database.py
@@ -23,6 +23,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Base class for models
 Base = declarative_base()
 
+
 # Dependency for FastAPI
 def get_db():
     db = SessionLocal()
